88a261ade8cfadf67b8c5aa81a1fbc9889fba9a3
Mirim <Mirim@DESKTOP-MED021C>
2023-04-05 15:59:54 +0900

    feat: Write toString() on Point class

---
+++ Pro02_Package/src/pack02/Point.java
@@ -0,0 +1,15 @@
+package pack02;
+
+public class Point {
+	private int x; //2
+	private int y; //3
+	
+	public Point(int x,int y) {
+		this.x=x;
+		this.y=y;
+	}
+	//재정의 
+	public String toString() {
+		return "Poin("+x+","+y+")";
+	}
+}
---
+++ Pro02_Package/src/pack02/ToStringEx.java
@@ -0,0 +1,13 @@
+package pack02;
+
+public class ToStringEx {
+
+	public static void main(String[] args) {
+		Point p=new Point(2,3);
+		System.out.println(p.toString());
+		System.out.println(p); //.toString() 생략
+		System.out.println(p+"입니다");
+
+	}
+
+}
