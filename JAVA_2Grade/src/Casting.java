163b3fe82247d4bd027a568e7f595905b63080fd
Mirim <Mirim@DESKTOP-MED021C>
2023-04-05 15:17:40 +0900

    init commit

---
+++ JAVA_2Grade/src/Casting.java
@@ -0,0 +1,13 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=123,b;
+		double y;
+		double a=1234.56;
+		y=x; //자동형변환(upcasting)
+		b=(int)a; //강제형변환(downcasting)
+	}
+
+}
