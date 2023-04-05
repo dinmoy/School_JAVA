24dbe54fe11e8694ef00092a0db92f97f6e9e91e
Mirim <Mirim@DESKTOP-MED021C>
2023-04-05 15:58:20 +0900

    feat: package example

---
+++ Pro02_Package/src/app/GoodCalc.java
@@ -0,0 +1,29 @@
+package app;
+
+import lib.Calculator; //폴더가 다르면 import 해야함
+
+public class GoodCalc extends Calculator{
+	
+	@Override 
+	public int add(int a,int b) {
+		return a+b;
+	}
+	@Override
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	@Override
+	public double average(int[] a) { //int[] a=new int{2,3,4}와 같은 표현
+		double sum=0;
+		for(int i=0;i<a.length;i++) 
+			sum+=a[i];
+		return sum/a.length;
+	}
+	
+	public static void main(String args[]) {
+		GoodCalc c=new GoodCalc();
+		System.out.println(c.add(2, 3));
+		System.out.println(c.subtract(2, 3));
+		System.out.println(c.average(new int[] {2,3,4})); //배열은 객체이므로  new를 써야함
+	}
+}
---
+++ Pro02_Package/src/lib/Calculator.java
@@ -0,0 +1,9 @@
+package lib;
+
+
+public abstract class Calculator {
+	public abstract int add(int a,int b); //추상 메서드
+	public abstract int subtract(int a,int b); 
+	public abstract double average(int[] a);
+	
+}
