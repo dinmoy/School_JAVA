b2f513d7ccb434023e90427356810fb91c2f4578
Mirim <Mirim@DESKTOP-MED021C>
2023-04-05 15:55:52 +0900

    feat: An example of inheriting a class at the same time after implementing an interface

---
+++ JAVA_2Grade/src/in11/PhoneInterface.java
@@ -0,0 +1,11 @@
+package in11;
+
+public interface PhoneInterface {
+	int TIMEOUT=10000; //public static final이 생략됨--상수
+	void sendCall(); //abstract이 생략됨 -- 추상메서드
+	void receiveCall();
+	
+	default void printLogo() {
+		System.out.println("** Phone **");
+	}
+}
