0bc5710243c7f44872bbe68da56765763681157f
Mirim <Mirim@DESKTOP-MED021C>
2023-04-05 15:54:56 +0900

    feat: Interface implementation

---
+++ JAVA_2Grade/src/in10/InterfaceEx.java
@@ -0,0 +1,14 @@
+package in10;
+
+public class InterfaceEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SamsungPhone phone=new SamsungPhone();
+		phone.printLogo();
+		phone.sendCall();
+		phone.receiveCall();
+		phone.flash();
+	}
+
+}
